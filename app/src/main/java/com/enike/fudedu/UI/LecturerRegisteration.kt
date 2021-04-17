e20dc02cb82cc616854077b46cc4e1a634485c11
ENIKE BRAIMOH <richardbraimoh@gmail.com>
2021-04-17 16:05:05 +0100

    finished registeration pages

--- app/src/main/java/com/enike/fudedu/UI/LecturerRegisteration.kt
+++ app/src/main/java/com/enike/fudedu/UI/LecturerRegisteration.kt
@@ -1,11 +1,11 @@
 package com.enike.fudedu.UI
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.enike.fudedu.R
 import com.enike.fudedu.databinding.FragmentLecturerRegisterationBinding
