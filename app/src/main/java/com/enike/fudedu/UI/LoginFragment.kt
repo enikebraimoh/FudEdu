0ae61847b00bdba7941f2d4e2435541877ade62f
ENIKE BRAIMOH <richardbraimoh@gmail.com>
2021-04-18 15:49:19 +0100

    finished registeration pages

--- app/src/main/java/com/enike/fudedu/UI/LoginFragment.kt
+++ app/src/main/java/com/enike/fudedu/UI/LoginFragment.kt
@@ -1,8 +1,11 @@
 package com.enike.fudedu.UI
 
+import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -14,6 +17,8 @@ import com.enike.fudedu.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/enike/fudedu/UI/StudentRegisteration.kt
+++ app/src/main/java/com/enike/fudedu/UI/StudentRegisteration.kt
@@ -1,19 +1,28 @@
 package com.enike.fudedu.UI
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.enike.fudedu.R
 import com.enike.fudedu.databinding.FragmentStudentRegisterBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class StudentRegisteration : Fragment() {
 
     private lateinit var binding: FragmentStudentRegisterBinding
+    private lateinit var Aauth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -21,10 +30,11 @@ class StudentRegisteration : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_student_register, container, false)
+        Aauth = Firebase.auth
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_student_register, container, false)
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,9 +42,29 @@ class StudentRegisteration : Fragment() {
         binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
         binding.toolbar.setNavigationOnClickListener { view ->
             binding.toolbar.findNavController()?.popBackStack()
+
+        }
+
+        binding.registerStudentsBnt.setOnClickListener {
+            createAccount(binding.emailfield.text.toString(), binding.passwordfield.text.toString())
         }
 
 
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Toast.makeText(requireContext(), email, Toast.LENGTH_SHORT).show()
+        Aauth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("eshoo", "createUserWithEmail:success")
+                        val user = Aauth.currentUser
+                    } else {
+                        Log.d("eshoo", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(requireContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 
 }
