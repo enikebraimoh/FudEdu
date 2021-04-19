6f7d42511f6ba3be98ac3a8fecc0e0ca1eb11859
ENIKE BRAIMOH <richardbraimoh@gmail.com>
2021-04-20 00:46:13 +0100

    finished registeration view model

--- app/src/main/java/com/enike/fudedu/UI/Login/LoginFragment.kt
+++ app/src/main/java/com/enike/fudedu/UI/Login/LoginFragment.kt
@@ -1,11 +1,8 @@
-package com.enike.fudedu.UI
+package com.enike.fudedu.UI.Login
 
-import android.app.AlertDialog
-import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -17,8 +14,6 @@ import com.enike.fudedu.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
-
-    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/enike/fudedu/UI/Registeration/StudentReg/StudentsDetailsModel.kt
@@ -0,0 +1,10 @@
+package com.enike.fudedu.UI.Registeration.StudentReg
+
+data class StudentsDetailsModel(val username: String,
+                                val firstname: String,
+                                val lastname: String,
+                                val email: String,
+                                val Gender: String,
+                                val faculty: String,
+                                val Department: String,
+                                val password: String)
